7986c432b640eb5ec0c54e9286f91c174fe24627
caarlosdamian <dapecarlos@gmail.com>
2021-11-10 20:17:18 -0600

    Add counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Counter } from "./bases/Counter";
+import { CounterBy } from "./bases/CounterBy";
+
 function App() {
   return (
     <>
       <h1>React</h1>
       <hr />
+      <Counter initialValue={15} />
+      <CounterBy />
     </>
   );
 }
