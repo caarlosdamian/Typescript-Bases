2743b97033d75404b78c5ba4c655962eed996efe
caarlosdamian <dapecarlos@gmail.com>
2021-11-11 18:59:12 -0600

    Add gsap animation

---
+++ src/bases/CounterEffect.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
+const MAXIMUN_COUNT = 10;
+
+export const CounterEffect = () => {
+  const [counter, setCounter] = useState(5);
+  const counterElement = useRef<HTMLHeadingElement>(null);
+  useEffect(() => {
+    if (counter < 10) return;
+    console.log("Numero maximo");
+
+    const tl = gsap.timeline();
+
+    tl.to(counterElement.current, { y: -10, duration: 0.2, ease: "ease.out" });
+    tl.to(counterElement.current, {
+      y: 0,
+      duration: 1,
+      ease: "bounce.out",
+    });
+  }, [counter]);
+
+  const handleClick = () => {
+    setCounter((prev) => Math.min(prev + 1, MAXIMUN_COUNT));
+    return;
+  };
+  return (
+    <>
+      <h1>CounterEffect:</h1>
+      <h2 ref={counterElement}>{counter}</h2>
+      <button onClick={handleClick}>+1</button>
+    </>
+  );
+};
